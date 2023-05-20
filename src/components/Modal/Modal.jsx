a6657dc8ed1a4c5f94b19b1456e231ac7927d934
Alexon7 <alenkaalexon@gmail.com>
2023-05-20 13:33:28 +0200

    fix modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import css from './Modal.module.css';
 
-export const Modal = (onImageClick, largeImgUrl) => {
+export const Modal = ({ largeImgUrl, onImageClick }) => {
   useEffect(() => {
     const handleKeyDown = e => {
       if (e.code === 'Escape') {
